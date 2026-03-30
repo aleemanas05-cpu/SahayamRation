e5d1b4558cc665dc6e8dfbc3b0b88097e383e179
aleemanas05-cpu <aleemanas05@gmail.com>
2026-03-30 15:55:42 +0530

    Creating main component for SahayamRation app.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to Sahayam Ration</h1>
+            <p>Voice-enabled ration ordering service</p>
+            {/* Components such as VoiceInput, OrderList can be added here */}
+        </div>
+    );
+};
+
+export default App;
